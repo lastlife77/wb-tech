6489b1f88a284303fd51c5c4439ddc8ea3219b44
lastlife77 <iliya090202@gmail.com>
2025-11-04 11:22:23 +0300

    l1.24:init

---
+++ l1/1.24/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	p1 := NewPoint(3, 4)
+	p2 := NewPoint(6, 8)
+	fmt.Println(p1.Distance(p2))
+	fmt.Println(p2.Distance(p1))
+}
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{
+		x: x,
+		y: y,
+	}
+}
+
+func (p1 *Point) Distance(p2 *Point) float64 {
+	return math.Sqrt(math.Pow(p1.x-p2.x, 2) + math.Pow(p1.y-p2.y, 2))
+}
